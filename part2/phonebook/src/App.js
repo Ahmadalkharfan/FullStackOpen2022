7d02d0dda727a275d5afeb1242bd294f5b885993
Ahmad Alkharfan <41472767+Ahmadalkharfan@users.noreply.github.com>
2022-10-30 16:25:14 +0400

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,12 +5,18 @@ const Person = ({ person }) => <div><b>{person.name}</b> <b>{person.number}</b><
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', 
-      number: '040-1234567'}
+    {
+      name: 'Arto Hellas',
+      number: '040-1234567'
+    }
   ])
-
+  const [search, setSearch] = useState({
+    filter: '',
+    list: []
+  })
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -41,26 +47,46 @@ const App = () => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+
+    const results = persons.filter(person => {
+      if (event.target.value === "") return persons
+      return person.name.toLowerCase().includes(event.target.value.toLowerCase())
+    })
+    setSearch({
+      query: event.target.value,
+      list: results
+    })
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={newFilter}
+        onChange={handleFilterChange} /></div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName}
             onChange={handleNameChange} />
         </div>
         <div>number: <input value={newNumber}
-            onChange={handleNumberChange} /></div>
+          onChange={handleNumberChange} /></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
-          <Person key={person.name} person={person} />
-        )}
+        {search.query === '' ?
+          persons.map(person =>
+            <Person key={person.name} person={person} />
+          ) //if the query is not empty
+          : search.list.map(person =>
+            <Person key={person.name} person={person} />
+          )}
       </ul>
     </div>
   )
