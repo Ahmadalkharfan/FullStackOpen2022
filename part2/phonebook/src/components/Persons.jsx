b845e036bb66e4534a17543ece437aa276ad1e40
Ahmad Alkharfan <41472767+Ahmadalkharfan@users.noreply.github.com>
2022-11-02 23:31:18 +0400

    2.17: Phonebook step9
    added window.confirm

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -7,7 +7,7 @@ const Persons = ({search,persons,deletePerson}) => {
                 {search.query === '' ?
                     persons.map(person =>
                         <div key={person.name}>
-                            <b>{person.name}</b> <b>{person.number}</b> <button onClick={() => deletePerson(person.id)}>delete</button>
+                            <b>{person.name}</b> <b>{person.number}</b> <button onClick={() => {if(window.confirm(`Delete ${person.name}?`))deletePerson(person.id)}}>delete</button>
                         </div>
                     ) //if the query is not empty
                     : search.list.map(person =>
