d5dd789e674d2a403565edc834581eb45c2235da
Ahmad Alkharfan <41472767+Ahmadalkharfan@users.noreply.github.com>
2023-02-20 20:34:42 +0400

    3.2: Phonebook backend step2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -31,6 +31,13 @@ app.get('/', (request, response) => {
     response.json(persons)
   })
   
+  app.get('/info', (request, response) => {
+    
+    response.send('Phonebook has info for ' + persons.length + '<br> <br>' + new Date() );
+    
+    console.log(timestamps + ' From routes/index.js')
+  })
+
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
