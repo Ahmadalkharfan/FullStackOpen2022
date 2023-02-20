ed9326a045304e5cc3c8758fcb14ca3146afd983
Ahmad Alkharfan <41472767+Ahmadalkharfan@users.noreply.github.com>
2023-02-20 21:14:00 +0400

    3.4: Phonebook backend step4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -41,6 +41,13 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
+
 app.get('/info', (request, response) => {
     response.send('Phonebook has info for ' + persons.length + '<br> <br>' + new Date());
 })
