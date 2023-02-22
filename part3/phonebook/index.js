e5d32012666bf57dca99829793a70ce4fe76af31
Ahmad Alkharfan <41472767+Ahmadalkharfan@users.noreply.github.com>
2023-02-22 15:22:43 +0400

    fix

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -36,9 +36,6 @@ let persons = [
         "number": "39-23-6423122"
     }
 ]
-app.get('/', (request, response) => {
-    response.send('<h1>Phone Book</h1>')
-})
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
