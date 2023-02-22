4201a926a829cb0a280859428f3ddba40d916776
Ahmad Alkharfan <41472767+Ahmadalkharfan@users.noreply.github.com>
2023-02-22 15:17:23 +0400

    3.11 phonebook full stack

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -9,6 +9,7 @@ morgan.token('body', req => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '))
 
+app.use(express.static('build'))
 
 const cors = require('cors')
 app.use(cors())
